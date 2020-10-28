babeaa22a73a5318b1b89c2608fe18e261ca5c01
namnh@agiletech.vn <namnguyen@Nguyens-MacBook-Pro-3.local>
2020-10-28 17:20:13 +0700

    i18next

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 // import AppContainer from './routes';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import ElsewhereStore from '@src/store';
+import '@src/locales';
 
 export default function App() {
   return (
