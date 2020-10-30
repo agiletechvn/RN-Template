e4e2656d5c198baf2e41716595b6d49f01fd28b1
namnh@agiletech.vn <namnguyen@Nguyens-MacBook-Pro-3.local>
2020-10-30 16:41:19 +0700

    route

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
+import '@src/locales';
 import React from 'react';
-// import AppContainer from './routes';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import ElsewhereStore from '@src/store';
-import '@src/locales';
-import Home from '@src/screens/Home';
+import Routes from '@src/routes';
 
 export default function App() {
   return (
     <Provider store={ElsewhereStore.store}>
       <SafeAreaProvider>
         <PersistGate loading={null} persistor={ElsewhereStore.persistor}>
-          <Home />
+          <Routes />
         </PersistGate>
       </SafeAreaProvider>
     </Provider>
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '@src/screens/Home';
+
+const Stack = createStackNavigator();
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
